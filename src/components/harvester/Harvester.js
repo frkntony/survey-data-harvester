fbb00730ab01451df5c79f5b61eb1fc9e8ede3eb
frkntony <tony.elistratov@gmail.com>
2018-08-01 16:38:17 +0300

    front-end added

--- src/components/harvester/Harvester.js
+++ src/components/harvester/Harvester.js
@@ -16,7 +16,7 @@ firebase.initializeApp(config);
 export default class Harvester extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
       uid: uuid.v1(), // time-based
       fullName: '',
@@ -32,19 +32,19 @@ export default class Harvester extends Component {
 
   } // constructor 
 
-  
+
   handleSubmit = (e) => {
     e.preventDefault()
-    this.setState( {fullName: this.refs.name.value}, () => console.log(this.state) )
+    this.setState({ fullName: this.refs.name.value }, () => console.log(this.state))
   }
 
   handleQuestions = (e) => {
     e.preventDefault()
-    this.setState( {
+    this.setState({
       answers: {
-        answer1 : this.refs.formQ1.value,
-        answer2 : this.refs.formQ2.value,
-        answer3 : this.refs.formQ3.value
+        answer1: this.refs.formQ1.value,
+        answer2: this.refs.formQ2.value,
+        answer3: this.refs.formQ3.value
       }
     }, () => {
       firebase.database().ref('surveyAnswers/' + this.state.uid).set({
@@ -52,9 +52,11 @@ export default class Harvester extends Component {
         fullName: this.state.fullName,
         answers: this.state.answers
       })
+      console.log('record added to firebase')
     })
+    this.setState( {isSubmitted: true} )
   }
-  
+
   render() {
 
     let nameGreetings;
@@ -62,30 +64,38 @@ export default class Harvester extends Component {
 
     if (this.state.fullName === '' && this.state.isSubmitted === false) {
       nameGreetings =
-      <div>
-        <h1>What's your name, buddy?</h1>
+
         <form onSubmit={this.handleSubmit}>
-          <input type='text' placeholed='enter your name' ref='name'/>
-        </form>
-      </div>;
+          <div className='form-group'>
+            <label htmlFor='formName'>What's your name, buddy?</label>
+            <br />
+            <input className='form-control' id='formName' type='text' placeholed='enter your name' ref='name' />
+          </div>
+        </form>;
+
       formQuestions = ''
     } else if (this.state.fullName !== '' && this.state.isSubmitted === false) {
+      
       nameGreetings = <h1>Welcome to personal data harvester, {this.state.fullName} </h1>
-      formQuestions = <div>
-
+      
+      formQuestions =
         <form onSubmit={this.handleQuestions}>
-          <label htmlFor='formQ1'><p>Where do you live?</p></label>
-          <input id='formQ1' ref='formQ1' type='text' placeholder='I live...' />
-          <br />
-          <label htmlFor='formQ2'><p>What are your guilty pleasures?</p></label>
-          <input id='formQ2' ref='formQ2' type='text' placeholder='My guilty pleasures are...' />
-          <br />
-          <label htmlFor='formQ3'><p>How much money do you make?</p></label>
-          <input id='formQ3' ref='formQ3' type='text' placeholder='I make...' />
-          <button type='submit'>Submit</button>
+          <div className='form-group'>
+            <label htmlFor='formQ1'><p>Where do you live?</p></label>
+            <input id='formQ1' className='form-control' ref='formQ1' type='text' placeholder='I live...' />
+            <br />
+            <label htmlFor='formQ2'><p>What are your guilty pleasures?</p></label>
+            <input id='formQ2' className='form-control' ref='formQ2' type='text' placeholder='My guilty pleasures are...' />
+            <br />
+            <label htmlFor='formQ3'><p>How much money do you make?</p></label>
+            <input id='formQ3' className='form-control' ref='formQ3' type='text' placeholder='I make...' />
+            <button className='btn' type='submit'>Submit</button>
+          </div>
         </form>
 
-      </div>
+    } else if (this.state.isSubmitted === true) {
+      nameGreetings = <p>Your personal data will be proceded</p>
+      formQuestions = <img src='images/checkmark.png' />
     }
 
     return (
